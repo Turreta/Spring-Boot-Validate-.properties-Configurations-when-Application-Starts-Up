f5d77ab6248f2f77165e8e80218246c08d723278
Turreta.com <Turreta@users.noreply.github.com>
2017-08-06 01:26:54 +0800

    Update ComTurretaSpringbootValidatePropertiesApplication.java

--- src/main/java/com/turreta/springboot/validate/properties/ComTurretaSpringbootValidatePropertiesApplication.java
+++ src/main/java/com/turreta/springboot/validate/properties/ComTurretaSpringbootValidatePropertiesApplication.java
@@ -9,15 +9,15 @@ public class ComTurretaSpringbootValidatePropertiesApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(ComTurretaSpringbootValidatePropertiesApplication.class, args);
+//		SpringApplication.run(ComTurretaSpringbootValidatePropertiesApplication.class, args);
 
-//		ApplicationContext context = SpringApplication.run(ComTurretaSpringbootValidatePropertiesApplication.class, args);
-//
-//		Module1ConfigBean m1 = context.getBean(Module1ConfigBean.class);
-//		System.out.print(m1);
-//
-//
-//		Module2ConfigBean m2 = context.getBean(Module2ConfigBean.class);
-//		System.out.print(m2);
+		ApplicationContext context = SpringApplication.run(ComTurretaSpringbootValidatePropertiesApplication.class, args);
+
+		Module1ConfigBean m1 = context.getBean(Module1ConfigBean.class);
+		System.out.print(m1);
+
+
+		Module2ConfigBean m2 = context.getBean(Module2ConfigBean.class);
+		System.out.print(m2);
 	}
 }
